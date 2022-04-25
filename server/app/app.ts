0dd4d5580ee21eb424afde1beb5e34fb58050dc0
Jobs <yukeli@whale.im>
2022-04-26 02:03:08 +0800

    feat: upload single file

--- server/app/app.ts
+++ server/app/app.ts
@@ -1,35 +1,49 @@
 import express = require('express');
 import path = require('path');
 import bodyParser = require('body-parser');
+import fs = require("fs")
+import multer = require('multer')
+
+const port = 3001;
+const ALLOW_ORIGIN_LIST = ['http://localhost:3000'];
+const defaultPath = './upload/';
+const BASIC_URL = `http://localhost:${port}/`
+const uploadDir = path.join(__dirname, defaultPath);
 
 const app: express.Application = express();
-const port = 3001;
-const ALLOW_ORIGIN_LIST = ['http://localhost:3000', 'http://www.jobsofferings.cn'];
+
+const upload = multer({
+  dest: uploadDir,
+  limits: {
+    files: 9,
+    fieldSize: 2 * 1024 * 1024 // 2 MB
+  }
+})
+
+app.use(upload.any()) // any表示任意类型的文件
+app.use(express.static(path.join(__dirname, defaultPath)));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.all('*', function (req, res, next) {
   if (ALLOW_ORIGIN_LIST.includes(req.headers.origin || '')) {
-    res.header('Access-Control-Allow-Origin', req.headers.origin); //当允许携带cookies此处的白名单不能写’*’
-    res.header('Access-Control-Allow-Headers', 'content-type,Content-Length, Authorization,Origin,Accept,X-Requested-With'); //允许的请求头
-    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT'); //允许的请求方法
+    res.header('Access-Control-Allow-Origin', req.headers.origin); // 当允许携带cookies此处的白名单不能写’*’
+    res.header('Access-Control-Allow-Headers', 'content-type,Content-Length, Authorization,Origin,Accept,X-Requested-With'); // 允许的请求头
+    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT'); // 允许的请求方法
     res.header('Access-Control-Allow-Credentials', 'true');
   }
   next();
 });
 
-app.use(express.static(path.join(__dirname, 'upload')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.post('/login', (req, res) => {
-  const { username, password } = req.body
-  if (username && password) {
-    
-  } else {
+app.post('/upload', upload.single('file'), (req: any, res) => {
+  req.files.map((file: any) => {
+    let oldName = file.path;
+    let newName = file.path + path.parse(file.originalname).ext;
+    fs.renameSync(oldName, newName);
     res.send({
-      flag: false,
-      msg: '参数错误'
+      url: BASIC_URL + path.basename(newName),
     })
-  }
+  })
 })
 
 app.use((error: Error, req: any, res: any, next: Function) => {
